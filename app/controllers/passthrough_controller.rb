d2eba5e86d94bf40262d6cd74c9063b3c884fd70
helsont <helsont@gmail.com>
2015-11-04 20:05:30 -0500

    End error

--- app/controllers/passthrough_controller.rb
+++ app/controllers/passthrough_controller.rb
@@ -5,6 +5,7 @@ class PassthroughController < ApplicationController
 			redirect_to new_user_session_path
 		else
 			redirect_to student_home_path
+		end
     # unless user_signed_in?
     # 	redirect_to new_user_session_path
     # elsif !@user.nil?
