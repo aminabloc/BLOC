7e94b4a6ef31416c6d7259a0366c22dfd56e78c0
helsont <helsont@gmail.com>
2015-11-04 20:02:24 -0500

    Updated redirection for logged in students

--- app/controllers/passthrough_controller.rb
+++ app/controllers/passthrough_controller.rb
@@ -1,12 +1,20 @@
 class PassthroughController < ApplicationController
   def index
     
-    if @user && current_user.role == "company" 
-      redirect_to company_home_path
-    elsif @user && current_user.role == "student"
-      redirect_to student_home_path 
-    else
-      redirect_to student_home_path
-    end
+		unless user_signed_in?
+			redirect_to new_user_session_path
+		else
+			redirect_to student_home_path
+    # unless user_signed_in?
+    # 	redirect_to new_user_session_path
+    # elsif !@user.nil?
+    # 	redirect_to student_home_path
+    # if @user && current_user.role == "company" 
+    #   redirect_to company_home_path
+    # elsif @user && current_user.role == "student"
+    #   redirect_to student_home_path 
+    # else
+    #   redirect_to student_home_path
+    # end
   end
 end
