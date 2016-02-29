6266f5ab0ad6811628fa50fae81646be7de967a4
aminabloc <amina@ourbloc.co>
2016-02-29 19:08:14 +0000

    video scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :videos
   resources :contents
   devise_for :users
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118013646) do
+ActiveRecord::Schema.define(version: 20160229190208) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -258,6 +258,13 @@ ActiveRecord::Schema.define(version: 20151118013646) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "videos", force: :cascade do |t|
+    t.string   "wistia"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
