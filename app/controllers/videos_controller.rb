d9672cff81519f62b1012230c135db6030586309
aminabloc <amina@ourbloc.co>
2016-02-29 19:56:32 +0000

    styling changes to new & index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -69,6 +69,6 @@ class VideosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
-      params.require(:video).permit(:wistia, :description)
+      params.require(:video).permit(:wistia, :description, :title)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229190208) do
+ActiveRecord::Schema.define(version: 20160229191445) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -264,6 +264,7 @@ ActiveRecord::Schema.define(version: 20160229190208) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "title"
   end
 
   create_table "votes", force: :cascade do |t|
