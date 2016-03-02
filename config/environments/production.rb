60a484fa0c4f7807a45e3a0f59beb0fb9e486976
aminabloc <amina@ourbloc.co>
2016-03-02 22:16:40 +0000

    working on paperclip aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,13 @@ Rails.application.configure do
 
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :bucket => ENV['S3_AWS_BUCKET'],
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
 
   config.cache_classes = true
 
